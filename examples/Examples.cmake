08b6c105a41f9661e1f2dc1f5a8572a1d3c3bc84
Luis Viveros <luis.viveros.escamilla@continental-corporation.com>
2024-06-12 16:59:21 -0600

    removed feedthrough

--- examples/Examples.cmake
+++ examples/Examples.cmake
@@ -189,7 +189,7 @@ if (${FMI_VERSION} EQUAL 3)
 endif()
 
 # Examples
-set(MODEL_NAMES FORD Dahlquist Feedthrough VanDerPol)
+set(MODEL_NAMES FORD Dahlquist VanDerPol)
 
 if (${FMI_VERSION} EQUAL 1)
     if (${FMI_TYPE} STREQUAL CS)
